14056ff9efeccf30a1f1b71e2fadf9ac232e5ae8
Kyle Zhou <kylezhou2002@hotmail.com>
2015-11-15 09:47:14 -0500

    time for a very long shot

--- src/messages-steg.js
+++ src/messages-steg.js
@@ -1,17 +1,41 @@
 var user1 = "Takashi";
 var user2 = "Calvin";
 
+var decodeQueue = []
+
+var image_urls = ["http://img11.deviantart.net/9dd3/i/2013/081/d/9/stego_dino_paper_by_apofiss-d54ehuu.jpg",
+					"http://img11.deviantart.net/9dd3/i/2013/081/d/9/stego_dino_paper_by_apofiss-d54ehuu.jpg"];
+
 //If user submits the form
 function sendMsg(){
     var clientmsg = $("#inputForm").val();
-    $.post("http://192.241.249.103/send.php", {text: clientmsg, name: user1, type: "text"});              
-    $("#inputForm").attr("value", "");
-    getChatLog();
+    var wcc = new WolframCloudCall();
+
+	wcc.insertMessage(image, message, 
+		function(result) { 
+			$.post("http://192.241.249.103/send.php", {text: result, name: user1, type: "img"});              
+		    $("#inputForm").attr("value", "");
+		    getChatLog();
+		});
     return false;
+}
+
+function getRandomImage(){
+	var index = Math.floor(Math.random() * 50);
+
+	if(index > 50){
+		index = 1;
+	} else {
+		index = 0;
+	}
+
+	return image_urls[index];
+
 }
 
 function getChatLog(){
 	$.get("http://192.241.249.103/index.php", function(data){
+		decodeQueue = []; // potential race condition
 		var parsedData = JSON.parse(data);
 		$("#msgContainer").empty();
 		for(var i = 0; i < parsedData.length; i++){
@@ -20,9 +44,9 @@ function getChatLog(){
 				var outputData = currentObj['data'];
 			} else {
 				// we have an image on our hands
-				//var outputData = "<img src=data:image/jpg;base64," + currentObj['data'] + "\">";
-				var outputData = '<img src="data:image/jpg;base64,' + currentObj['data'] + '" class="decodedMessages" alt="hey" style="width: 100px; height: 150px; margin-top: 0px;">';
-
+				//var decodeData = atob(currentObj['data']);
+				decodeQueue.push(currentObj['data']);
+				var outputData = '<a href="#" class="clickable" id="' + i + '"><img src="data:image/jpg;base64,' + currentObj['data'] + '" class="decodedMessages" alt="hey" style="width: 100px; height: 150px; margin-top: 0px;"></a>';
 			}
 			console.log("sender",currentObj['sender']);
 			console.log(outputData);
@@ -54,11 +78,25 @@ function getChatLog(){
 	});
 }
 
+function handleClick(caller){
+	var id = caller.attr("id");
+	var rawData = decodeQueue[int(id)];
+	var unb64 = atob(rawData);
+
+	var wcc = new WolframCloudCall();
+	wcc.extractImage(unb64, function(result) {
+		alert(result);
+	});
+}
+
 $(function(){
 	$("#send_button").click(function(event){
 		event.preventDefault();
 		console.log("Send button clicked");
 		sendMsg();
+	});
+	$(".clickable").click(function(event){
+		handleClick(this);
 	});
 	getChatLog();
 	setInterval (getChatLog, 10000);
---
+++ src/wolfram.js
@@ -0,0 +1,80 @@
+/* 
+JavaScript EmbedCode usage:
+
+var wcc = new WolframCloudCall();
+wcc.insertMessage(image, message, function(result) { console.log(result); });
+*/
+ 
+var WolframCloudCall;
+
+(function() {
+WolframCloudCall = function() {	this.init(); };
+
+var p = WolframCloudCall.prototype;
+
+p.init = function() {};
+
+p._createCORSRequest = function(method, url) {
+	var xhr = new XMLHttpRequest();
+	if ("withCredentials" in xhr) {
+		xhr.open(method, url, true);
+	} else if (typeof XDomainRequest != "undefined") {
+		xhr = new XDomainRequest();
+		xhr.open(method, url);
+	} else {
+		xhr = null;
+	}
+	return xhr;
+};
+
+p._encodeArgs = function(args) {
+	var argName;
+	var params = "";
+	for (argName in args) {
+		params += (params == "" ? "" : "&");
+		params += encodeURIComponent(argName) + "=" + encodeURIComponent(args[argName]);
+	}
+	return params;
+};
+
+p._auxCall = function(url, args, callback) {
+	var params = this._encodeArgs(args);
+	var xhr = this._createCORSRequest("post", url);
+	if (xhr) {
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.setRequestHeader("EmbedCode-User-Agent", "EmbedCode-JavaScript/1.0");
+		xhr.onload = function() {
+			if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+				callback(xhr.responseText);
+			} else {
+				callback(null);
+			}
+		};
+		xhr.send(params);
+	} else {
+		throw new Error("Could not create request object.");
+	}
+};
+
+#formerly called p.call
+p.insertMessage = function(image, message, callback) {
+	var url = "http://www.wolframcloud.com/objects/user-b156354d-c537-4c00-91ef-31bb69876c20/insertion";
+	var args = {image: image, message: message};
+	var callbackWrapper = function(result) {
+		if (result === null) callback(null);
+		else callback(result);
+	};
+	this._auxCall(url, args, callbackWrapper);
+};
+})();
+
+p.extractImage = function(image, callback) {
+	var url = "http://www.wolframcloud.com/objects/user-b156354d-c537-4c00-91ef-31bb69876c20/extraction";
+	var args = {image: image};
+	var callbackWrapper = function(result) {
+		if (result === null) callback(null);
+		else callback(result);
+	};
+	this._auxCall(url, args, callbackWrapper);
+};
+})();
