9d0925cf94b42cdceab6dc0f877d185383975203
Kyle Zhou <kylezhou2002@hotmail.com>
2015-11-15 09:00:43 -0500

    sending messages kinda works

--- src/messages.js
+++ src/messages.js
@@ -3,40 +3,48 @@ var user2 = "Calvin";
 
 //If user submits the form
 function sendMsg(){
-  $("#send_button").click(function(){   
     var clientmsg = $("#inputForm").val();
-    $.post("post.php", {text: clientmsg});              
+    $.post("http://192.241.249.103/send.php", {text: clientmsg, name: user1, type: "text"});              
     $("#inputForm").attr("value", "");
     getChatLog();
     return false;
-  });
 }
 
 function getChatLog(){
 	$.get("http://192.241.249.103/index.php", function(data){
-		var parsedData = json.parse(data);
+		var parsedData = JSON.parse(data);
 		$("#msgContainer").empty();
 		for(var i = 0; i < parsedData.length; i++){
-			var outputData = "";
-			if(parsedData['iora'] == 'f'){
-				outputData = parsedData['data'];
+			var currentObj = parsedData[i];
+			if(currentObj['iora'] == 'f'){
+				var outputData = currentObj['data'];
 			} else {
 				// we have an image on our hands
-				outputData = "<img src=data:image/jpg;base64," + parsedData['data'] + "\">";
+				var outputData = "<img src=data:image/jpg;base64," + currentObj['data'] + "\">";
 			}
-			if(parsedData['sender'] == user1){
+			console.log("sender",currentObj['sender']);
+			console.log(outputData);
+			if(currentObj['sender'] == user1){
 				// generate user1 stuff
-				$.get("templates/message_item_ours.html", function(response){
-					// set attributes
-					var replaced = response.replace("[BODY]", outputData);
-					$("#msgContainer").append(replaced);
+				$.ajax({
+					url: "templates/message_item_ours.html",
+					success: function(response){
+						// set attributes
+						var replaced = response.replace("[BODY]", outputData);
+						$("#msgContainer").append(replaced);
+					},
+					async: false
 				});
 			} else {
 				// generate user2 stuff
-				$.get("templates/message_item_theirs.html", function(response){
-					// set attributes
-					var replaced = response.replace("[BODY]", outputData);
-					$("#msgContainer").append(replaced);
+				$.ajax({
+					url: "templates/message_item_theirs.html",
+					success: function(response){
+						// set attributes
+						var replaced = response.replace("[BODY]", outputData);
+						$("#msgContainer").append(replaced);
+					},
+					async: false
 				});
 			}
 
@@ -45,5 +53,11 @@ function getChatLog(){
 }
 
 $(function(){
-	setInterval (loadLog, 2000);
+	$("#send_button").click(function(event){
+		event.preventDefault();
+		console.log("Send button clicked");
+		sendMsg();
+	});
+	getChatLog();
+	setInterval (getChatLog, 10000);
 });
